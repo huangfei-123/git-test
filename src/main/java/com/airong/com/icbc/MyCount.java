403868c8a29b35a194b33cb09681798a9f80415e
HF <jkl@qq.com>
2021-11-12 16:39:44 +0800

    third commit

--- src/main/java/com/airong/com/icbc/MyCount.java
+++ src/main/java/com/airong/com/icbc/MyCount.java
@@ -3,5 +3,6 @@ package com.airong.com.icbc;
 public class MyCount {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
